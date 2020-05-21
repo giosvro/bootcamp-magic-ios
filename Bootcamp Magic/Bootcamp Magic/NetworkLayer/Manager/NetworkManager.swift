5baecb516e81ae4aed123c7de1fdefd9388d2610
Giovanni Severo Barros <giosvro@gmail.com>
2020-05-21 16:19:11 -0300

    Cria Network Response
    Foi criado o Network Response para ajudar a identificar e lidar com possíveis erros

--- Bootcamp Magic/Bootcamp Magic/NetworkLayer/Manager/NetworkManager.swift
+++ Bootcamp Magic/Bootcamp Magic/NetworkLayer/Manager/NetworkManager.swift
@@ -13,3 +13,41 @@ struct NetworkManager {
     static let APIKey = "API_KEY"
     private let router = Router<MagicApi>()
 }
+
+enum NetworkResponse: String {
+    case success
+    case authenticationError = "You need to be authenticated first"
+    case badRequest = "We could not process that action"
+    case failed = "Network request failed"
+    case noData = "The requested resource could not be found"
+    case unableToDecode = "We could not decote the response"
+    case forbidden = "You exceeded the rate limit"
+    case internalServerError = "We had a problem with our server. Please try again later"
+    case serviceUnavailable = "We are temporarily offline for maintenance. Please try again later"
+    
+    fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
+        switch response.statusCode {
+        case 400:
+            return .failure(NetworkResponse.badRequest.rawValue)
+        case 403:
+            return .failure(NetworkResponse.forbidden.rawValue)
+        case 404:
+            return .failure(NetworkResponse.noData.rawValue)
+        case 500:
+            return .failure(NetworkResponse.internalServerError.rawValue)
+        case 503:
+            return .failure(NetworkResponse.serviceUnavailable.rawValue)
+        case 401...500:
+            return .failure(NetworkResponse.authenticationError.rawValue)
+        case 501...599:
+            return .failure(NetworkResponse.badRequest.rawValue)
+        default:
+            return .failure(NetworkResponse.failed.rawValue)
+        }
+    }
+}
+
+enum Result<String> {
+    case sucess
+    case failure(String)
+}
