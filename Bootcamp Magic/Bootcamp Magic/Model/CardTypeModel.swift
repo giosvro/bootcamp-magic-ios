863fa7d4310f9fd7a193129e9fc5b1ea47b39051
Giovanni Severo Barros <giosvro@gmail.com>
2020-05-22 11:30:45 -0300

    Cria Card Type Model

--- Bootcamp Magic/Bootcamp Magic/Model/CardTypeModel.swift
+++ Bootcamp Magic/Bootcamp Magic/Model/CardTypeModel.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct CardTypeApiResponse {
+    var types: [String]
+}
+
+extension CardTypeApiResponse: Decodable {
+    
+    private enum SetApiResponseCodingKeys: String, CodingKey {
+        case types
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: SetApiResponseCodingKeys.self)
+        types = try container.decode([String].self, forKey: .types)
+    }
+}
