aed82faeb96f2b746e7d6164a4a4d4d15a4ee901
Giovanni Severo Barros <giosvro@gmail.com>
2020-06-03 15:41:59 -0300

    Inicializa Pods

---
+++ Bootcamp Magic/Pods/Target Support Files/Pods-Bootcamp Magic-Bootcamp MagicUITests/Pods-Bootcamp Magic-Bootcamp MagicUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bootcamp_Magic_Bootcamp_MagicUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Bootcamp_Magic_Bootcamp_MagicUITests
+@end
